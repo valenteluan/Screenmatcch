bca75d3a94377b0bbb2e5e30d2098f162f55de68
Luan Valente <luanbvalente@hotmail.com>
2024-07-05 13:31:51 -0300

    Projeto Criado

---
+++ src/main/java/br/com/alura/screenmatcch/ScreenmatcchApplication.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatcch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatcchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatcchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/alura/screenmatcch/ScreenmatcchApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatcch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatcchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
