9c0afd036de7c81f733ba800c37ab841a2b29f7c
Luan Valente <luanbvalente@hotmail.com>
2024-07-07 19:15:34 -0300

    Recebendo Dados no Controller

--- src/main/java/br/com/alura/screenmatcch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatcch/controller/FilmeController.java
@@ -1,7 +1,9 @@
 package br.com.alura.screenmatcch.controller;
 
+import br.com.alura.screenmatcch.domain.filme.DadosCadastroFilme;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -13,5 +15,10 @@ public class FilmeController {
         return "filmes/formulario";
     }
 
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilme dados) {
+        System.out.println(dados);
+        return "filmes/formulario";
+    }
 
 }
---
+++ src/main/java/br/com/alura/screenmatcch/domain/filme/DadosCadastroFilme.java
@@ -0,0 +1,4 @@
+package br.com.alura.screenmatcch.domain.filme;
+
+public record DadosCadastroFilme(String nome, Integer duracao, Integer ano, String genero) {
+}
