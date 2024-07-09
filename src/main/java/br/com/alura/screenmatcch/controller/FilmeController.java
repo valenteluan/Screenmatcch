9f607eb2059bc368334bec1dab5c5cf5afd35a8a
Luan Valente <luanbvalente@hotmail.com>
2024-07-09 13:15:04 -0300

    Acessando variáveis na página com Thymeleaf

--- src/main/java/br/com/alura/screenmatcch/controller/FilmeController.java
+++ src/main/java/br/com/alura/screenmatcch/controller/FilmeController.java
@@ -3,6 +3,7 @@ package br.com.alura.screenmatcch.controller;
 import br.com.alura.screenmatcch.domain.filme.DadosCadastroFilme;
 import br.com.alura.screenmatcch.domain.filme.Filme;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +23,8 @@ public class FilmeController {
     }
 
     @GetMapping
-    public String carregaPaginaListagem() {
+    public String carregaPaginaListagem(Model model) {
+        model.addAttribute("lista", filmes);
         return "filmes/listagem";
     }
 
