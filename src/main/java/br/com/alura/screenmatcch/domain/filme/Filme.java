7bdaec802c5f0d2545dc65efc3f46f239166192e
Luan Valente <luanbvalente@hotmail.com>
2024-07-10 17:20:40 -0300

    Mapeando informações

--- src/main/java/br/com/alura/screenmatcch/domain/filme/Filme.java
+++ src/main/java/br/com/alura/screenmatcch/domain/filme/Filme.java
@@ -1,7 +1,14 @@
 package br.com.alura.screenmatcch.domain.filme;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="filmes")
 public class Filme {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private Integer duracaoEmMinutos;
     private Integer anoLancamento;
@@ -12,6 +19,10 @@ public class Filme {
         this.duracaoEmMinutos = dados.duracao();
         this.anoLancamento = dados.ano();
         this.genero = dados.genero();
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public String getNome() {
